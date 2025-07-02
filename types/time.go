758f9fd5240c0ce9542c3dc64f3926a730b4801b
taodev <taodev@qq.com>
2025-07-02 13:24:57 +0800

    feat: 添加时间类型及格式化功能
    增加了一个新的时间类型，并实现了时间字符串的解析与格式化功能，以支持更灵活的时间处理。

--- types/time.go
+++ types/time.go
@@ -1,1 +1,17 @@
 package types
+
+import "time"
+
+type Time int64
+
+func (t *Time) Format(v string) {
+	tm, err := time.ParseInLocation("2006-01-02", v, time.Local)
+	if err != nil {
+		panic(err)
+	}
+	*t = Time(tm.Unix())
+}
+
+func (t Time) String() string {
+	return time.Unix(int64(t), 0).Format("2006-01-02")
+}
