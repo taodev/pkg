6d9c1493be2a9f6c4f545aaddf58f0351e194212
taodev <taodev@qq.com>
2025-07-09 08:42:12 +0800

    feat: 支持多种 YAML 二进制标签及解析格式
    增加对 base64、hex 等多种二进制 YAML 标签与序列格式的解析，提升二进制数据兼容性和灵活性。

--- types/binary.go
+++ types/binary.go
@@ -2,9 +2,22 @@ package types
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 
 	"gopkg.in/yaml.v3"
+)
+
+const (
+	// BinaryTag 二进制编码标签
+	BinaryTag = "!!binary"
+	// BinaryStrTag 二进制字符串编码标签
+	BinaryStrTag = "!!str"
+	// BinaryBase64Tag 二进制Base64编码标签
+	BinaryBase64Tag = "!!base64"
+	// BinaryHexTag 二进制Base64原始编码标签
+	BinaryHexTag = "!!hex"
+	BinarySeqTag = "!!seq"
 )
 
 type Binary []byte
@@ -13,16 +26,23 @@ type Binary []byte
 func (b Binary) MarshalYAML() (any, error) {
 	return yaml.Node{
 		Kind:  yaml.ScalarNode,
-		Tag:   "!!binary",
 		Value: base64.StdEncoding.EncodeToString(b),
 	}, nil
 }
 
 func (b *Binary) UnmarshalYAML(value *yaml.Node) error {
-	if value.Tag != "!!binary" && value.Tag != "!!str" {
+	var decoded []byte
+	var err error
+	switch value.Tag {
+	case BinaryTag, BinaryStrTag, BinaryBase64Tag:
+		decoded, err = base64.StdEncoding.DecodeString(value.Value)
+	case BinaryHexTag:
+		decoded, err = hex.DecodeString(value.Value)
+	case BinarySeqTag:
+		err = value.Decode(&decoded)
+	default:
 		return fmt.Errorf("unexpected tag: %s", value.Tag)
 	}
-	decoded, err := base64.StdEncoding.DecodeString(value.Value)
 	if err != nil {
 		return err
 	}
---
+++ types/binary_test.go
@@ -0,0 +1,58 @@
+package types
+
+import (
+	"encoding/hex"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
+)
+
+func TestBinary(t *testing.T) {
+	const (
+		base64Data = "SGVsbG8gQmluYXJ5IQ=="
+		hexData    = "48656c6c6f2042696e61727921"
+		strData    = "Hello Binary!"
+	)
+	var b Binary
+	err := b.UnmarshalYAML(&yaml.Node{
+		Tag:   "!!binary",
+		Value: base64Data,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, []byte(strData), []byte(b))
+	assert.Equal(t, hexData, hex.EncodeToString(b))
+	assert.Equal(t, base64Data, b.String())
+
+	err = b.UnmarshalYAML(&yaml.Node{
+		Tag:   "!!hex",
+		Value: hexData,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, []byte(strData), []byte(b))
+
+	yamlStruct := &struct {
+		Value1 Binary `yaml:"value1"`
+		Value2 Binary `yaml:"value2"`
+		Value3 Binary `yaml:"value3"`
+		Value4 Binary `yaml:"value4"`
+		Value5 Binary `yaml:"value5"`
+	}{}
+	data, err := os.ReadFile("data/binary.yaml")
+	require.NoError(t, err)
+	err = yaml.Unmarshal(data, yamlStruct)
+	require.NoError(t, err)
+	assert.Equal(t, []byte(strData), yamlStruct.Value1.Bytes())
+	assert.Equal(t, []byte(strData), yamlStruct.Value2.Bytes())
+	assert.Equal(t, []byte(strData), yamlStruct.Value3.Bytes())
+	assert.Equal(t, []byte(strData), yamlStruct.Value4.Bytes())
+	assert.Equal(t, []byte(strData), yamlStruct.Value5.Bytes())
+
+	data, err = yaml.Marshal(yamlStruct.Value1)
+	require.NoError(t, err)
+	log.Printf("%s", data)
+	assert.Contains(t, string(data), base64Data)
+}
