14bacc4b61c6e15a6dd2a810def16465681cc81f
taodev <taodev@qq.com>
2025-07-21 16:40:40 +0800

    docs(config): 为JSON和YAML函数添加中文注释说明

--- config/json.go
+++ config/json.go
@@ -5,6 +5,10 @@ import (
 	"os"
 )
 
+// LoadJSON 函数用于从指定文件中读取 JSON 数据并将其反序列化到给定的变量中。
+// 参数 filename 是要读取的 JSON 文件的路径。
+// 参数 v 是用于存储反序列化结果的变量指针，类型为 any。
+// 返回值为错误信息，如果读取或反序列化过程中出现错误，则返回相应的错误；否则返回 nil。
 func LoadJSON(filename string, v any) error {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -13,6 +17,10 @@ func LoadJSON(filename string, v any) error {
 	return json.Unmarshal(data, v)
 }
 
+// SaveJSON 函数用于将给定的变量序列化为 JSON 格式并保存到指定文件中。
+// 参数 filename 是要保存的 JSON 文件的路径。
+// 参数 v 是要序列化的变量，类型为 any。
+// 返回值为错误信息，如果序列化或写入文件过程中出现错误，则返回相应的错误；否则返回 nil。
 func SaveJSON(filename string, v any) error {
 	data, err := json.Marshal(v)
 	if err != nil {
--- config/yaml.go
+++ config/yaml.go
@@ -6,6 +6,10 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// LoadYAML 函数用于从指定文件中读取 YAML 数据并将其反序列化到给定的变量中。
+// 参数 filename 是要读取的 YAML 文件的路径。
+// 参数 v 是用于存储反序列化结果的变量指针，类型为 any。
+// 返回值为错误信息，如果读取或反序列化过程中出现错误，则返回相应的错误；否则返回 nil。
 func LoadYAML(filename string, v any) error {
 	data, err := os.ReadFile(filename)
 	if err != nil {
@@ -14,6 +18,10 @@ func LoadYAML(filename string, v any) error {
 	return yaml.Unmarshal(data, v)
 }
 
+// SaveYAML 函数用于将给定的变量序列化为 YAML 格式并保存到指定文件中。
+// 参数 filename 是要保存的 YAML 文件的路径。
+// 参数 v 是要序列化的变量，类型为 any。
+// 返回值为错误信息，如果序列化或写入文件过程中出现错误，则返回相应的错误；否则返回 nil。
 func SaveYAML(filename string, v any) error {
 	data, err := yaml.Marshal(v)
 	if err != nil {
